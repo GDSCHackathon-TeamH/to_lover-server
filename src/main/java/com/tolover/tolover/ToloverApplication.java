abf82288d4f464eaad4e7ed312547e9801025116
junho <junholim98@gmail.com>
2024-01-12 16:05:58 +0900

    first commit

---
+++ src/main/java/com/tolover/tolover/ToloverApplication.java
@@ -0,0 +1,13 @@
+package com.tolover.tolover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToloverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToloverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tolover/tolover/ToloverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tolover.tolover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToloverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
