e8a476a550225e4590e2ef1ab01014cb42d2eedb
junho <junholim98@gmail.com>
2024-01-12 16:19:15 +0900

    프로젝트 기본 세팅2

--- src/main/java/com/tolover/tolover/controller/UserController.java
+++ src/main/java/com/tolover/tolover/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.tolover.tolover.controller;
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
+@RequestMapping("/api/user")
 public class UserController {
 
 
---
+++ src/main/java/com/tolover/tolover/service/KakaoService.java
@@ -0,0 +1,4 @@
+package com.tolover.tolover.service;
+
+public class KakaoService {
+}
